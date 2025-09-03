5f5305a7b8623161edd67f95263e819dec632b70
Xiang Li <xiangli5699@gmail.com>
2025-09-03 13:43:45 -0400

    Add logs for loading revue

--- service/src/main/java/org/cbioportal/genome_nexus/service/remote/RevueDataFetcher.java
+++ service/src/main/java/org/cbioportal/genome_nexus/service/remote/RevueDataFetcher.java
@@ -1,27 +1,33 @@
 package org.cbioportal.genome_nexus.service.remote;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.cbioportal.genome_nexus.model.VuesJsonRecord;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
 @Component
 public class RevueDataFetcher {
 
+    private static final Log LOG = LogFactory.getLog(RevueDataFetcher.class);
     public static VuesJsonRecord[] getRevueData(String revueUrl) {
         RestTemplate restTemplate = new RestTemplate();
         // Fetch the JSON data
+        LOG.debug("Fetching reVUE data from URL: " + revueUrl);
         ResponseEntity<String> response = restTemplate.getForEntity(revueUrl, String.class);
-        String json = response.getBody();
+        String json = response.getBody(); 
         Gson gson = new Gson();
-        try {;
+        try {
             // Convert the JSON data to a VuesJsonRecord[] array
             VuesJsonRecord[] records = gson.fromJson(json, VuesJsonRecord[].class);
+            LOG.debug("Successfully parsed reVUE data, number of records: " + (records != null ? records.length : 0));
             return records;
         } catch (JsonSyntaxException e) {
-            System.err.println("Error getting JSON file: " + e.getMessage());
+            LOG.error("Error getting JSON file: " + e.getMessage());
             return null;
         }
     }
